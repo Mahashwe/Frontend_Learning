8c71d24886cd9ae68f979f23547df75abb3ebb76
MahaSwetha <maha.tomholland@gmail.com>
2026-02-03 12:31:32 +0530

    Refactor index.js: Add examples of normal, factory, and constructor functions

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,34 @@
-let n="maha";
-console.log("hello "+ n);
+//normal function (function inside a object) - returns value only 
+// let circle={
+//     radius:1,
+//     location:{
+//         x:1,
+//         y:1  
+//     },
+//     draw: function(){
+//         console.log("draw")
+//     }
+// };
+
+//factory function (function that returns object)
+// function circle(radius){
+//     return{
+//     radius,
+//     hi : function(){
+//         console.log("this is test");
+//     }
+// };
+// }
+
+// const a = circle(7);
+// console.log(a)
+
+// constructor function (function that is used to create object using 'new' keyword)
+function Circle(radius){
+    this.radius=radius; 
+    this.draw=function(){
+        console.log("draw from constructor function");
+    }
+}           
+const b = new Circle(9);
+console.log(b); 
